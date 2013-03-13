3b79829c6235b013be1ede771b36c911af8f485f
Arek Turlewicz <aturlewicz@mediweb.jp>
2013-03-13 17:55:17 +0900

    Add label options and bind to knockout.

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,5 +1,5 @@
 module FormsHelper
-  def bootstrap_input(field_name, &block)
-    render "shared/bootstrap_input", field_name: field_name, block: block
+  def bootstrap_input(field_name, options={}, &block)
+    render "shared/bootstrap_input", field_name: field_name, options: options, block: block
   end
 end
