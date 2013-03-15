e15c26685c73c2567a9e7d43fb5f06348d0c32bf
Arek Turlewicz <aturlewicz@mediweb.jp>
2013-03-15 17:44:42 +0900

    Send json to controller.

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,4 +1,8 @@
 class FormsController < ApplicationController
   def index
   end
+  def create
+    Rails.logger.debug "received: #{params.to_yaml}"
+    render :json => {errors:{first_name:["two long"], age:["to old"]}}.to_json
+  end
 end
