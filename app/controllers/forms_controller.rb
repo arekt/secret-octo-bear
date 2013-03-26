43485d9c3f64f4c8c8a313fbadda7b8cc588ccb4
Arek Turlewicz <aturlewicz@mediweb.jp>
2013-03-26 21:24:32 +0900

    First level errors support.

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,9 +1,14 @@
 class FormsController < ApplicationController
   def index
-    @person = {:first_name => "John", :last_name => "Smith", :age => 45, :details => { hobby: "Books", work: "PR"}}
+    @person = {:first_name => "John", :last_name => "Smith", :age => 45, :details => { hobby: "Movies", work: "PR", some_field: "asdfas"}, :errors => [:first_name => "aaaaa"]}
   end
   def create
     Rails.logger.debug "received: #{params.to_yaml}"
-    render :json => {errors:{first_name:["two long"], age:["to old"]}}.to_json
+    @person = params[:person]
+    @person[:last_name] += "a"
+    @person[:errors] = []
+    @person[:errors] << {:last_name => "is to short"}
+    @person[:details][:errors] = [{:hobby => "this is work, not hobby"}]
+    render :json => @person.to_json
   end
 end
